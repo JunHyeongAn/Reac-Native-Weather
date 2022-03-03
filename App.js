4c19d67f3f0058bbddeeb82ab19e63665659c03d
JunHyeongAn <sure083080@gmail.com>
2022-03-03 14:30:28 +0900

    weather app

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get("window").width;
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
+        <View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+
+        <View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+
+        <View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+
+        <View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container : {
+    flex: 1, 
+    backgroundColor: 'tomato'
   },
-});
+  city : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName : {
+    fontSize: 60,
+    fontWeight: "500"
+  },
+  weather : {
+
+  },
+  day : {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+
+  },
+  temp : {
+    fontSize: 178
+  },
+  description : {
+    marginTop: 50,
+    fontSize: 60
+  }
+})
